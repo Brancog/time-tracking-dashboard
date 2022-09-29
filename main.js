83a57c5205ff3f993b4eccedb18b84dec76746c1
Branko Gambiroza <brankogambiroza@pop-os.localdomain>
2022-09-29 17:12:55 +0200

    project finished

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const cardTimeFrame = document.querySelectorAll(".card__timeframe");
 
+// ACTIVATED TIME FRAMES
+cardTimeFrame.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    for (const el of cardTimeFrame) {
+      if (el.classList.contains("selected")) {
+        el.classList.remove("selected");
+      }
+    }
+    element.classList.add("selected");
+  });
+});
+
+// HOVER STATES ON CARD BACKGROUND AND DOT BUTTONS
